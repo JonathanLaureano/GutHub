cddbc01a30ef78fe16c4f504112ffaa0419c2ffb
Henry Miller <mail.henrym+git@gmail.com>
2022-03-22 21:14:42 -0400

    updated index and app

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,17 +1,18 @@
 import './App.css';
-import { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { Route, Switch } from "react-router-dom";
+import axios from 'axios';
 
 function App() {
 
   return (
-    <BrowserRouter>
       <div className="App">
         <Switch>
-        
+          <Route>
+
+          </Route>
         </Switch>
       </div>
-    </BrowserRouter>
   );
 }
 
