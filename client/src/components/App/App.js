66a79cba98a8051e361e3016070aa4140495ea5f
Henry Miller <mail.henrym+git@gmail.com>
2022-03-22 22:07:52 -0400

    adding home page and mixing page

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import React, { useState, useEffect } from 'react'
 import { Route, Switch } from "react-router-dom";
 import axios from 'axios';
+import HomePage from '../homepage/HomePage'
+import MixPage from '../MixPage/MixPage'
+
 
 function App() {
 
   return (
       <div className="App">
+        {/* <div>This is the App Page</div> */}
         <Switch>
-          <Route>
-
+          <Route exact path='/'>
+              <HomePage/>
+          </Route>
+          <Route path='/mix'>
+              <MixPage/>
           </Route>
         </Switch>
       </div>
