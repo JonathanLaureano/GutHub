7c1fd70b3318586460b20a25d3dd4bf5fee50e1f
Henry Miller <mail.henrym+git@gmail.com>
2022-03-24 10:28:43 -0400

    updated home page styling, colors

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -7,17 +7,23 @@ export default function HomePage(){
     document.title = 'GutHub - Home'
     return(
         <React.Fragment>
-            <h3 className='subtitle'>Welcome to</h3>
-            {/* <h1 className='title'>GutHub</h1> */}
-            <div className="logo"><b>G<span>ut</span>H<span>u</span>b</b></div>
-            <input 
-                type='text'
-                placeholder='Search For A Drink'
-                autoComplete='off'
-                cursor='pointer'
-                className='home-search'
-                >
-            </input>
+            {/* <div className='parallax'> */}
+                <h3 className='subtitle'>Welcome to</h3>
+                <div className="logo"><b>G<span>ut</span>H<span>u</span>b</b></div>
+                <input 
+                    type='text'
+                    placeholder='Search For A Drink'
+                    autoComplete='off'
+                    cursor='pointer'
+                    className='home-search'
+                    >
+                </input>
+            {/* </div>
+            <div style={{height:"1000px", backgroundColor:"black", fontSize:"36px", color:'#fff'}}>
+Scroll Up and Down this page to see the parallax scrolling effect.
+This div is just here to enable scrolling.
+Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div> */}
         </React.Fragment>
     )
 }
