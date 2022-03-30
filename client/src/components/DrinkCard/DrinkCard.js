195317e4f69ed168ead39e3aa6ffcc36ee9f000a
Henry Miller <mail.henrym+git@gmail.com>
2022-03-29 21:48:51 -0400

    updated drink page code formatting

--- client/src/components/DrinkCard/DrinkCard.js
+++ client/src/components/DrinkCard/DrinkCard.js
@@ -3,39 +3,36 @@ import './DrinkCard.css';
 
 export default function DrinkCard({drink}){
     const images = require.context('../Images-Resized',true);
-
-    let imgClass = drink.name=='Margarita'?'drink-card-image margarita':'drink-card-image'
-
-    let imgClass2;
+    let imgClass;
 
     switch (drink.name) {
         case "Margarita":
-            imgClass2='drink-card-image margarita'
+            imgClass='drink-card-image margarita'
             break;
         case "Bloody Mary":
-            imgClass2='drink-card-image bloodymary'
+            imgClass='drink-card-image bloodymary'
             break;
         case 'Caipirinha':
-            imgClass2='drink-card-image caipirinha'
+            imgClass='drink-card-image caipirinha'
             break;
         case 'Old Fashioned':
-            imgClass2='drink-card-image oldfashioned'
+            imgClass='drink-card-image oldfashioned'
             break;
         case 'Gin & Tonic':
-            imgClass2='drink-card-image gnt';
+            imgClass='drink-card-image gnt';
             break;
         case 'The Godfather':
-            imgClass2='drink-card-image godfather';
+            imgClass='drink-card-image godfather';
             break;
         default:
-            imgClass2='drink-card-image'
+            imgClass='drink-card-image'
             
     }
     
     return(
         <React.Fragment>
             <div className="drink-card-container">
-                <img className={imgClass2} src={images('./'+drink.image_url)}/>
+                <img className={imgClass} src={images('./'+drink.image_url)}/>
                 <div className="drink-card-text-info">
                     <div className="drink-card-name">{drink.name}</div>
                     <div className="drink-card-ingredient">{drink.recipes[0].ingredient.name}</div>
