955072656edfda5f839ac4b395b2bfca0acefef0
Henry Miller <mail.henrym+git@gmail.com>
2022-03-30 22:26:27 -0400

    modal prototype and demo testing in favorites

--- client/src/components/Favorites/FavoritesPage.js
+++ client/src/components/Favorites/FavoritesPage.js
@@ -2,13 +2,16 @@ import './FavoritesPage.css';
 import React, { useState, useEffect } from 'react'
 import { Route, Switch } from "react-router-dom";
 import axios from 'axios';
+import MixMatchModal from '../Modals/MixMatch/MixMatchModal';
 
 export default function FavoritesPage(){
     document.title = 'GutHub - Your Favorites'
     return(
         <React.Fragment>
-            <div>This is your FavoritesPage</div>
-            <a className="" href="/profile">Back</a>
+            <div className="modal-curtain"></div>
+            <div className='mock-container'>
+                <MixMatchModal />
+            </div>
         </React.Fragment>
     )
 }
---
+++ client/src/components/Modals/MixMatch/MixMatchModal.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../Modal.css';
+import './MixMatchModal.css';
+
+export default function MixMatchModal(){
+    return(
+            <div className="modal-card">
+                <div className="modal-top"> 
+                    <div className="modal-left"> </div>
+                    <div className="modal-right"> </div>
+                </div>
+                <div className="modal-bot"> </div>
+            </div>
+    )
+}
