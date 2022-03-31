017b6edf410d1137b36fc142b406cf2f03b05cd2
Henry Miller <mail.henrym+git@gmail.com>
2022-03-30 23:50:13 -0400

    preliminary drink modal styling finished;

--- client/src/components/Favorites/FavoritesPage.js
+++ client/src/components/Favorites/FavoritesPage.js
@@ -11,11 +11,68 @@ export default function FavoritesPage(){
     function clickAway(){
         setModalActive(false);
     }
+
+    let mix = {
+        "id": 21,
+        "name": "Pink Lady",
+        "image_url": "PinkLady.png",
+        "user": {
+        "id": 1,
+        "first_name": "Henry",
+        "last_name": "Miller",
+        "username": "mozart@guthub.com",
+        "password_digest": "$2a$12$VjBcmnhikxhC8teqwwRYGuEgFdzUa.DXouhpxB/Sw/ZT1Nsxn/k2q",
+        "image_url": "",
+        "description": ""
+        },
+        "recipes": [
+        {
+        "id": 88,
+        "parts": 1.33,
+        "ingredient": {
+        "id": 7,
+        "name": "Gin",
+        "image_url": "Gin.png",
+        "ingredient_type": "Alcohol"
+        }
+        },
+        {
+        "id": 89,
+        "parts": 0.5,
+        "ingredient": {
+        "id": 15,
+        "name": "Lime Juice",
+        "image_url": "LimeJuice.png",
+        "ingredient_type": "Liquid"
+        }
+        },
+        {
+        "id": 90,
+        "parts": 0.5,
+        "ingredient": {
+        "id": 18,
+        "name": "Grenadine",
+        "image_url": "Grenadine.png",
+        "ingredient_type": "Liquid"
+        }
+        },
+        {
+        "id": 91,
+        "parts": 1,
+        "ingredient": {
+        "id": 41,
+        "name": "Cherry",
+        "image_url": "Cherry.png",
+        "ingredient_type": "Solid"
+        }
+        }
+        ]
+        }
     return(
         <React.Fragment>
             <div className="modal-curtain"></div>
             <div className='mock-container'>
-                <MixMatchModal onDrop={clickAway}/>
+                <MixMatchModal onDrop={clickAway} mix={mix}/>
             </div>
         </React.Fragment>
     )
--- client/src/components/Modals/MixMatch/MixMatchModal.js
+++ client/src/components/Modals/MixMatch/MixMatchModal.js
@@ -5,22 +5,25 @@ import './MixMatchModal.css';
 export default function MixMatchModal({mix}){
     const images = require.context('../../Images-Resized',true);
 
-    // let recipesToDisplay = mix.recipes.map(recipe=>{
-    //     return(
-    //         <div className="modal-drink-recipe"> </div>
-    //     )
-    // })
+    let recipesToDisplay = mix.recipes.map(recipe=>{
+        return(
+            <div className="modal-drink-recipe"><b>{recipe.ingredient.name}</b>: {recipe.parts}</div>
+        )
+    })
 
     return(
             <div className="modal-card">
                 <div className="modal-top"> 
                     <div className="modal-left"> 
-                        <img src={images('./'+'PinkLady.png')} className="modal-drink-image"/>
+                        <img src={images('./'+mix.image_url)} className="modal-drink-image"/>
                     </div>
                     <div className="modal-right">   
-                        <div className="modal-drink-name">Pink Lady</div>
+                        <div className="modal-drink-name">{mix.name}</div>
                         <hr></hr>
                         <div className="modal-drink-recipe-title">Recipe: </div>
+                        <div className="modal-drink-recipe-wrapper">
+                            {recipesToDisplay}
+                        </div>
                     </div>
                 </div>
                 <div className="modal-bot"> 
