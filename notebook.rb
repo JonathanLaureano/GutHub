258ed87c747bf949e663a07b69d2c68e1e759a4f
Henry Miller <mail.henrym+git@gmail.com>
2022-04-02 01:56:15 -0400

    updated and fix the mix method to account for instances where mixes with additional recipes are posted

--- notebook.rb
+++ notebook.rb
@@ -1,4 +1,13 @@
-test={"mix":[
+testTrue={"mix":[
+    { "ingredient_id": 1, "parts": 1.33 },
+    { "ingredient_id": 2, "parts": 0.33 },
+    { "ingredient_id": 15, "parts": 0.66 },
+    { "ingredient_id": 19, "parts": 0.33 },
+    { "ingredient_id": 39, "parts": 1.0 },
+    { "ingredient_id": 24, "parts": 1.0 }
+]}
+
+testLong={"mix":[
     { "ingredient_id": 1, "parts": 1.33 },
     { "ingredient_id": 2, "parts": 0.33 },
     { "ingredient_id": 15, "parts": 0.66 },
@@ -7,6 +16,15 @@ test={"mix":[
     { "ingredient_id": 24, "parts": 1.0 },
     {"ingredient_id": 35, "parts": 1.3}
 ]}
+
+testShort={"mix":[
+    { "ingredient_id": 1, "parts": 1.33 },
+    { "ingredient_id": 2, "parts": 0.33 },
+    { "ingredient_id": 15, "parts": 0.66 },
+    { "ingredient_id": 19, "parts": 0.33 },
+    { "ingredient_id": 39, "parts": 1.0 },
+]}
+
 
 
 
@@ -20,4 +38,33 @@ Drink.all.map{|drink| drink.recipes.map{|recipe| mix.map{|ingred| recipe.ingredi
 
 Drink.all.filter{|drink| drink.recipes.map{|recipe| mix.map{|ingred| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}}
 
-Drink.all.map{|drink| drink.recipes.map{|recipe| posted_object[:mix].map{|ingred| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}}
+
+# WITH MAPPING -> TO SEE TRUE/FALSE INDEX LISTs
+# Will return drinks where all of the recipes can be found in the provided object but does not account for additonal recipes in the provided object
+def MixMatchMap obj
+    Drink.all.map{|drink| drink.recipes.map{|recipe| obj[:mix].map{|ingred| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}}
+end
+
+# Will return drinks where all of the recipes found in the provided can also be found in the drink but does not account for recipes that are in the drink and missing from the provided object 
+def MixMatchMapAlt obj
+    Drink.all.map{|drink| obj[:mix].map{|ingred| drink.recipes.map{|recipe| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}}
+end
+
+def MixMatchMapComplete obj
+    Drink.all.map{|drink| (drink.recipes.map{|recipe| obj[:mix].map{|ingred| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}) && (obj[:mix].map{|ingred| drink.recipes.map{|recipe| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}) }
+end
+
+
+# WITH FILTERS -> Return only the matching drink
+def MixMatchFilter obj
+    Drink.all.filter{|drink| drink.recipes.map{|recipe| obj[:mix].map{|ingred| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}}
+end
+
+# Will return drinks where all of the recipes found in the provided can also be found in the drink but does not account for recipes that are in the drink and missing from the provided object 
+def MixMatchFilterAlt obj
+    Drink.all.filter{|drink| obj[:mix].map{|ingred| drink.recipes.map{|recipe| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}}
+end
+
+def MixMatchFilterComplete obj
+    Drink.all.filter{|drink| (drink.recipes.map{|recipe| obj[:mix].map{|ingred| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}) && (obj[:mix].map{|ingred| drink.recipes.map{|recipe| recipe.ingredient_id == ingred[:ingredient_id] && recipe.parts == ingred[:parts]}.include? true}.all?{|item| item==true}) }
+end
