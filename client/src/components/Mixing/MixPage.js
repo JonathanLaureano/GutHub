0930ea6630763ff587fa056517a8994f4da1f031
Henry Miller <mail.henrym+git@gmail.com>
2022-04-02 03:06:01 -0400

    initial styling for tabs

--- client/src/components/Mixing/MixPage.js
+++ client/src/components/Mixing/MixPage.js
@@ -175,7 +175,10 @@ export default function MixPage({user,ingredients,setIngredients,showDrinkModal,
                 </div>
                 <div className='mix-page-ingredients-container'>
                     <div className='mix-page-ingredients-tab-holders'>
-
+                        <div className='mix-page-ingredients-type-tab'>ALL</div>
+                        <div className='mix-page-ingredients-type-tab'>ALCOHOL</div>
+                        <div className='mix-page-ingredients-type-tab'>LIQUIDS</div>
+                        <div className='mix-page-ingredients-type-tab'>SOLIDS</div>
                     </div>
                     <div className='mix-page-ingredients-holder' id='cards'>
                         {displayIngredients(ingredients)}
