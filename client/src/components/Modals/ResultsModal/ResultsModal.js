caf460ad1c38c36c46f8a0d45c83ce83666b0526
Henry Miller <mail.henrym+git@gmail.com>
2022-04-04 21:40:27 -0400

    added styling for recipes on resultmodal

--- client/src/components/Modals/ResultsModal/ResultsModal.js
+++ client/src/components/Modals/ResultsModal/ResultsModal.js
@@ -15,18 +15,18 @@ export default function ResultsModal({mixes,showResultsModal,setShowResultsModal
     let mixIngredients = mixes.mix;
 
     function ingredientCardsRow(data) {
-        return <div className='mix-page-ingredients-cards-container'>
+        return <div className='mix-page-ingredients-cards-container result-modal'>
             {data.map(mixIngredient => 
                 {
                     let fullIngredient= ingredients.filter(ingredient => ingredient.id == [mixIngredient["ingredient_id"]])[0] 
                     console.log(fullIngredient)
 
                 return (
-                    <div id={fullIngredient.id} className='ingredient-card-container'>
-                    <img className='ingredient-card-image' src={ingredImages('./' + fullIngredient.image_url)} />
-                    <div className="ingredient-card-text-info">
-                        <div className="ingredient-card-name">{fullIngredient.name}</div>
-                        <div className="ingredient-card-partsCount">
+                    <div id={fullIngredient.id} className='resultModal-ingredient-container'>
+                    <img className='resultModal-ingredient-image' src={ingredImages('./' + fullIngredient.image_url)} />
+                    <div className="resultModal-ingredient-text-info">
+                        <div className="resultModal-ingredient-name">{fullIngredient.name}</div>
+                        <div className="resultModal-ingredient-partsCount">
                     </div>
                     </div>
                     </div>
@@ -42,8 +42,12 @@ export default function ResultsModal({mixes,showResultsModal,setShowResultsModal
         <div className="modal-card">
             <div className="modal-top">
                 <div className="modal-left">
-                    {ingredientCardsRow(mixIngredients)}
+                    <div className='resultModal-ingredients-title'>RECIPE:</div>
+                    <div className='resultModal-ingredients-wrapper' id='cards'>
+                        {ingredientCardsRow(mixIngredients)}
+                    </div>
                 </div>
+
                 <div className="modal-right">
                 </div>
             </div>
