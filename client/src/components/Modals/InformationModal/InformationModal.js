55cca1f345260681d3e2e70c729a1c009b028380
Henry Miller <mail.henrym+git@gmail.com>
2022-04-09 15:31:58 -0400

    minor rearranging of information modal

--- client/src/components/Modals/InformationModal/InformationModal.js
+++ client/src/components/Modals/InformationModal/InformationModal.js
@@ -20,13 +20,11 @@ export default function InformationModal({showInformationModal, setShowInformati
                     <li className='information-item-small'>RELATIVE - Finds all drinks with the provided ingredients regardless of the parts count</li>
                     <li className='information-item-small'>STRICT - Finds a drink with an exact match of the provided ingredients and parts count</li>
                     <li className='information-item'>Ingredient parts counts are measured in shots</li>
-                    <li className='information-item'>There is a max total parts count of 11 shots per recipe</li>
                     <li className='information-item-small'>1 shot is equivalent to 1.5 fl oz</li>
+                    <li className='information-item'>There is a max total parts count of 11 shots per recipe</li>
                     <li className='information-item-small'>Solid ingredients have a max of 1 parts count per ingredient</li>
                     <li className='information-item-small'>Alcohol and Liquid ingredients have a max of 5 parts count per ingredient</li>
                 </ul>
-
-
             </div>
             <div className="modal-bot">
                 <button className="close-button" onClick={handleClickCloseButton}>CLOSE</button>
