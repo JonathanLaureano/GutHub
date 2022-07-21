fa4bd2c316d978b6eb43b886803b508ced12313a
Henry Miller <mail.henrym+git@gmail.com>
2022-07-21 16:40:57 -0400

    add front end validation for ensuring users add a name for their drink

--- client/src/components/Modals/MixModal/MixModal.js
+++ client/src/components/Modals/MixModal/MixModal.js
@@ -1,62 +1,67 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import '../Modal.css';
 import './MixModal.css';
 
-export default function MixModal({user,drink,setShowMixModal,setMatchFound,ingredients,handleCreateDrinkSubmit}){
-    const drinkImgs = require.context('../../../img/drinks',true);
+export default function MixModal({ user, drink, setShowMixModal, setMatchFound, ingredients, handleCreateDrinkSubmit }) {
+    const drinkImgs = require.context('../../../img/drinks', true);
     const ingredientImgs = require.context('../../../img/ingredients', true);
 
-    let [drinkName,setDrinkName] = useState('')
+    let [drinkName, setDrinkName] = useState('')
 
-    function handleDrinkNameChange(e){
+    function handleDrinkNameChange(e) {
         setDrinkName(e.target.value);
     }
 
-    let recipesToDisplay = drink.recipes.map(recipe=>{
-            let fullIngredient = ingredients.filter(ingredient => ingredient.id == [recipe["ingredient_id"]])[0]
-            return (
-                <div className='mix-recipe-identity'>
-                    <div className='mix-recipe-nameandcount'>
+    let recipesToDisplay = drink.recipes.map(recipe => {
+        let fullIngredient = ingredients.filter(ingredient => ingredient.id == [recipe["ingredient_id"]])[0]
+        return (
+            <div className='mix-recipe-identity'>
+                <div className='mix-recipe-nameandcount'>
                     <img className='mix-recipe-image' src={ingredientImgs('./' + fullIngredient.image_url)} />
-                        <div className='mix-recipe'><b>~</b>{fullIngredient.name}: </div>
-                        <div className='mix-recipe-partsCount'><b>~</b>{recipe.parts}</div>
-                    </div>
+                    <div className='mix-recipe'><b>~</b>{fullIngredient.name}: </div>
+                    <div className='mix-recipe-partsCount'><b>~</b>{recipe.parts}</div>
                 </div>
-            )
-    
+            </div>
+        )
+
     })
 
-    function handleClickCloseButton(){
+    function handleClickCloseButton() {
         setShowMixModal(false);
         setMatchFound(false);
     }
 
-    function handleClickCreate(){
-        let newDrink= {...drink,
-            "name": drinkName
+    function handleClickCreate() {
+        if (drinkName === '') {
+            alert('Please enter a drink name')
+        } else {
+            let newDrink = {
+                ...drink,
+                "name": drinkName
+            }
+            handleCreateDrinkSubmit(newDrink)
         }
-        handleCreateDrinkSubmit(newDrink)
     }
 
-    return(
-            <div className="modal-card">
-                <div className="modal-top"> 
-                    <div className="modal-left"> 
-                        <img alt={drink.name} src={drinkImgs('./'+drink.image_url)} className="modal-drink-image"/>
-                    </div>
-                    <div className="modal-right">   
-                        <input type='text' placeholder='Enter A Drink Name' value={drinkName} onChange={handleDrinkNameChange} className='mix-modal-input'></input>
-                        <hr></hr>
-                        <div className="modal-drink-recipe-title">Recipe: </div>
-                        <div className="modal-drink-recipe-wrapper">
-                            {recipesToDisplay}
-                        </div>
-                    </div>
+    return (
+        <div className="modal-card">
+            <div className="modal-top">
+                <div className="modal-left">
+                    <img alt={drink.name} src={drinkImgs('./' + drink.image_url)} className="modal-drink-image" />
                 </div>
-                <div className="modal-bot"> 
-                    <button className="favorite-button" onClick={handleClickCreate}>CREATE DRINK</button>
-                    <button className="close-button" onClick={handleClickCloseButton}>CLOSE</button>
+                <div className="modal-right">
+                    <input type='text' placeholder='Enter A Drink Name' value={drinkName} onChange={handleDrinkNameChange} className='mix-modal-input'></input>
+                    <hr></hr>
+                    <div className="modal-drink-recipe-title">Recipe: </div>
+                    <div className="modal-drink-recipe-wrapper">
+                        {recipesToDisplay}
+                    </div>
                 </div>
             </div>
+            <div className="modal-bot">
+                <button className="favorite-button" onClick={handleClickCreate}>CREATE DRINK</button>
+                <button className="close-button" onClick={handleClickCloseButton}>CLOSE</button>
+            </div>
+        </div>
     )
 }
