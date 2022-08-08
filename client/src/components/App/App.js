d46792cfd54b6631621e15a6ce9f6a2bbb0eb9db
Henry Miller <mail.henrym+git@gmail.com>
2022-08-07 23:58:25 -0400

    refactor app

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -20,7 +20,7 @@ function App() {
 
   let history = useHistory();
   //User Auth:
-  
+
   // Sign Up:
   const [signUpFirstName, setSignUpFirstName] = useState("");
   const [signUpLastName, setSignUpLastName] = useState("");
@@ -29,41 +29,41 @@ function App() {
   const [signUpPasswordConfirmation, setSignUpPasswordConfirmation] = useState("");
   const [signUpImage, setSignUpImage] = useState("");
   const [signUpDesc, setSignUpDesc] = useState("");
-  
+
   // Log In:
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  
+
   // Signed In:
   const [signedIn, setSignedIn] = useState(false);
   const [user, setUser] = useState(null)
-  
+
   const [drinks, setDrinks] = useState([]);
   const [ingredients, setIngredients] = useState([]);
-  const [favorites,setFavorites] = useState([])
-  
+  const [favorites, setFavorites] = useState([])
+
   // Search
   const [searchActive, setSearchActive] = useState(false)
   const [searchParams, setSearchParams] = useState('');
   const [searchResults, setSearchResults] = useState(drinks)
-  
-  const [mixActive,setMixActive]= useState(false);
-  
-  
+
+  const [mixActive, setMixActive] = useState(false);
+
+
   // * Modals
   let [showSignInModal, setShowSignInModal] = useState(false);
-  
-  const [showResultsModal,setShowResultsModal] = useState(false);
-  const [mixResults,setMixResults] = useState({});
-  const [showMixModal,setShowMixModal] = useState(false);
-  const [mixToCreate,setMixToCreate] = useState({});
-  const [showProfileModal,setShowProfileModal] = useState(false);
-  const [showDrinkModal,setShowDrinkModal] = useState(false);
-  const [selectedDrink,setSelectedDrink] = useState({});
-  const [showInformationModal,setShowInformationModal] = useState(false);
+
+  const [showResultsModal, setShowResultsModal] = useState(false);
+  const [mixResults, setMixResults] = useState({});
+  const [showMixModal, setShowMixModal] = useState(false);
+  const [mixToCreate, setMixToCreate] = useState({});
+  const [showProfileModal, setShowProfileModal] = useState(false);
+  const [showDrinkModal, setShowDrinkModal] = useState(false);
+  const [selectedDrink, setSelectedDrink] = useState({});
+  const [showInformationModal, setShowInformationModal] = useState(false);
   let [matchFound, setMatchFound] = useState(false);
-  
-  
+
+
   function handleSearchSubmit(e) {
     e.preventDefault();
     let query = { "query": searchParams }
@@ -125,12 +125,12 @@ function App() {
       })
 
     axios.get('/drinks/1')
-    .then(r=>{
-      setSelectedDrink(r.data)
-    })
+      .then(r => {
+        setSelectedDrink(r.data)
+      })
 
     axios.get('/favorites')
-    .then(r=>setFavorites(r.data))
+      .then(r => setFavorites(r.data))
 
   }, [])
 
@@ -237,233 +237,239 @@ function App() {
     ScrollAction.scrollToBottom();
   }
 
-  function scrollTop(){
+  function scrollTop() {
     ScrollAction.scrollToTop();
   }
 
 
-  function handleCreateDrinkSubmit(newDrink){
-    axios.post('/mixcreate',newDrink)
-    .then(r=>{
-      setDrinks([...drinks,r.data])
-      setSelectedDrink(r.data);
-      setShowMixModal(false)
-      setShowDrinkModal(true)
-    })
-    .catch(function (error) {
-      if (error.response) {
-        console.log(error.response.data.errors);
-        alert(error.response.data.errors)
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-    });
-
-
-  fetch("/me")
-    .then((r) => {
-      if (r.ok) {
-        r.json().then((user) => {
-          setUser(user)
+  function handleCreateDrinkSubmit(newDrink) {
+    axios.post('/mixcreate', newDrink)
+      .then(r => {
+        setDrinks([...drinks, r.data])
+        setSelectedDrink(r.data);
+        setShowMixModal(false)
+        setShowDrinkModal(true)
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data.errors);
+          alert(error.response.data.errors)
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
         }
-        );
-      }
-    });
+      });
 
-  axios.get('/drinks')
-    .then(r => {
-      setDrinks(r.data)
-    })
+
+    fetch("/me")
+      .then((r) => {
+        if (r.ok) {
+          r.json().then((user) => {
+            setUser(user)
+          }
+          );
+        }
+      });
+
+    axios.get('/drinks')
+      .then(r => {
+        setDrinks(r.data)
+      })
   }
 
 
-  function handleFavoriteDrink(drink){
-    let drinkToFavorite = {"drink":drink.id}
-    axios.post('/favorite',drinkToFavorite)
-    .then(r=>{
-      setUser(r.data)
-    })
+  function handleFavoriteDrink(drink) {
+    let drinkToFavorite = { "drink": drink.id }
+    axios.post('/favorite', drinkToFavorite)
+      .then(r => {
+        setUser(r.data)
+      })
   }
 
-  function handleUnfavoriteDrink(drink){
-    let drinkToUnFavorite = {"drink":drink.id}
-    axios.post('/unfavorite',drinkToUnFavorite)
-    .then(r=>{
-      setUser(r.data)
-    })
+  function handleUnfavoriteDrink(drink) {
+    let drinkToUnFavorite = { "drink": drink.id }
+    axios.post('/unfavorite', drinkToUnFavorite)
+      .then(r => {
+        setUser(r.data)
+      })
   }
 
 
 
   function handleSignIn() {
-      setShowSignInModal(true)
+    setShowSignInModal(true)
   }
 
   function scrollTop() {
-      setSearchParams('')
-      ScrollAction.scrollToTop();
-      setTimeout(() => {
-          setSearchActive(false)
-      }, 750)
+    setSearchParams('')
+    ScrollAction.scrollToTop();
+    setTimeout(() => {
+      setSearchActive(false)
+    }, 750)
   }
 
   function scrollTopMix() {
-      //reset ingredients 
-      ScrollAction.scrollToTop();
-      setTimeout(() => {
-          setMixActive(false)
-      }, 750)
+    //reset ingredients 
+    ScrollAction.scrollToTop();
+    setTimeout(() => {
+      setMixActive(false)
+    }, 750)
   }
 
   function clickOffModal() {
-      setShowDrinkModal(false);
-      setShowProfileModal(false)
-      setShowMixModal(false);
-      setShowResultsModal(false);
+    setShowDrinkModal(false);
+    setShowProfileModal(false)
+    setShowMixModal(false);
+    setShowResultsModal(false);
   }
 
   function clickMixActivateButton() {
-      setMixActive(true)
-      ScrollAction.scrollToBottom();
+    setMixActive(true)
+    ScrollAction.scrollToBottom();
   }
 
   let modalActive = showDrinkModal || showProfileModal || showMixModal || showResultsModal || showInformationModal;
 
   let [mixes, setMixes] = useState({
-      "mix": [
-      ]
+    "mix": [
+    ]
   });
+
+  const displayHeader = signedIn ? <Header
+    signedIn={signedIn}
+    setSignedIn={setSignedIn}
+    showProfileModal={showProfileModal}
+    setShowProfileModal={setShowProfileModal} /> : null;
+
+  const subtitleClass = signedIn ? 'subtitle' : 'subtitle hidden';
+  const titleClass = signedIn && !searchActive && !mixActive ? "logo" : "logo off";
+  const displayHomeButtons = signedIn ? <div className='home-buttons-wrapper'>
+    <button className='home-mix-button' onClick={handleSearchSubmit}>Search For a Drink </button>
+    <button className='home-mix-button' onClick={clickMixActivateButton}>Make a Mix</button>
+  </div> : <button className='home-signIn' onClick={handleSignIn}>Sign In</button>;
+
+  const displaySignInModal = showSignInModal ? <SignInModal
+    username={username}
+    setUsername={setUsername}
+    password={password}
+    setPassword={setPassword}
+    handleLogInSubmit={handleLogInSubmit}
+    showSignInModal={showSignInModal}
+    setShowSignInModal={setShowSignInModal}
+    signUpFirstName={signUpFirstName}
+    setSignUpFirstName={setSignUpFirstName}
+    signUpLastName={signUpLastName}
+    setSignUpLastName={setSignUpLastName}
+    signUpUsername={signUpUsername}
+    setSignUpUsername={setSignUpUsername}
+    signUpPassword={signUpPassword}
+    setSignUpPassword={setSignUpPassword}
+    signUpPasswordConfirmation={signUpPasswordConfirmation}
+    setSignUpPasswordConfirmation={setSignUpPasswordConfirmation}
+    signUpImage={signUpImage}
+    setSignUpImage={setSignUpImage}
+    signUpDesc={signUpDesc}
+    setSignUpDesc={setSignUpDesc}
+    handleSignUpSubmit={handleSignUpSubmit}
+    handleDemoLogIn={handleDemoLogIn} /> : null;
+
+  const displayDrinkModal = showDrinkModal ? <div className='modal-container'>
+    <DrinkModal
+      drink={selectedDrink}
+      setShowDrinkModal={setShowDrinkModal}
+      setMatchFound={setMatchFound}
+      handleFavoriteDrink={handleFavoriteDrink}
+      handleUnfavoriteDrink={handleUnfavoriteDrink}
+      favorites={favorites}
+      setFavorites={setFavorites}
+      ingredients={ingredients} />
+  </div> : null;
+  const displayProfileModal = showProfileModal ? <div className='modal-container'>
+    <ProfileModal profile={user} setUser={setUser} setShowProfileModal={setShowProfileModal} handleLogOut={handleLogOut} drinks={drinks} setSelectedDrink={setSelectedDrink} setShowDrinkModal={setShowDrinkModal} />
+  </div> : null;
+  const displayMixModal = showMixModal ? <div className='modal-container'>
+    <MixModal user={user} setShowMixModal={setShowMixModal} drink={mixToCreate} ingredients={ingredients} handleCreateDrinkSubmit={handleCreateDrinkSubmit} />
+  </div> : null;
+  const displayResultsModal = showResultsModal ? <div className='modal-container'>
+    <ResultsModal
+      setShowResultsModal={setShowResultsModal}
+      mixResults={mixResults}
+      mixes={mixes}
+      ingredients={ingredients}
+      setSelectedDrink={setSelectedDrink}
+      setShowDrinkModal={setShowDrinkModal}
+      handleFavoriteDrink={handleFavoriteDrink}
+      handleUnfavoriteDrink={handleUnfavoriteDrink}
+      favorites={favorites}
+      setFavorites={setFavorites} />
+  </div> : null;
+  const displayInformationModal = showInformationModal ? <div className='modal-container'>
+    <InformationModal showInformationModal={showInformationModal} setShowInformationModal={setShowInformationModal} />
+  </div> : null;
+  const modalCurtain = modalActive ? <div onClick={clickOffModal} className="modal-curtain"></div> : null;
+
+  const displaySearchPage = searchActive ? <SearchPage
+    user={user}
+    searchActive={searchActive}
+    searchResults={searchResults}
+    setSearchResults={setSearchResults}
+    searchParams={searchParams}
+    setSearchParams={setSearchParams}
+    handleSearchChange={handleSearchChange}
+    handleSearchSubmit={handleSearchSubmit}
+    scrollTop={scrollTop}
+    showDrinkModal={showDrinkModal}
+    setShowDrinkModal={setShowDrinkModal}
+    selectedDrink={selectedDrink}
+    setSelectedDrink={setSelectedDrink}
+    showProfileModal={showProfileModal}
+    favorites={favorites}
+    setFavorites={setFavorites}
+    modalActive={modalActive} /> : null;
+
+  const displayMixPage = mixActive ? <MixPage
+    mixes={mixes}
+    setMixes={setMixes}
+    user={user}
+    matchFound={matchFound}
+    setMatchFound={setMatchFound}
+    ingredients={ingredients}
+    setIngredients={setIngredients}
+    showDrinkModal={showDrinkModal}
+    setShowDrinkModal={setShowDrinkModal}
+    selectedDrink={selectedDrink}
+    setSelectedDrink={setSelectedDrink}
+    scrollTopMix={scrollTopMix}
+    showProfileModal={showProfileModal}
+    mixToCreate={mixToCreate}
+    setMixToCreate={setMixToCreate}
+    showMixModal={showMixModal}
+    setShowMixModal={setShowMixModal}
+    showResultsModal={showResultsModal}
+    setShowResultsModal={setShowResultsModal}
+    mixResults={mixResults}
+    setMixResults={setMixResults}
+    modalActive={modalActive}
+    showInformationModal={showInformationModal}
+    setShowInformationModal={setShowInformationModal} /> : null;
 
   return (
     <div className="App">
-      {signedIn ? <Header
-        signedIn={signedIn}
-        setSignedIn={setSignedIn}
-        showProfileModal={showProfileModal}
-        setShowProfileModal={setShowProfileModal}
-      /> : null}
-     <React.Fragment>
-            <h3 className={signedIn ? 'subtitle' : 'subtitle hidden'}>Welcome to</h3>
-            <div className={signedIn && !searchActive && !mixActive ? "logo" : "logo off"}><b>G<span>ut</span>H<span>u</span>b</b></div>
-            <div className='home-buttons-wrapper'>
-                {signedIn ? <button className='home-mix-button' onClick={handleSearchSubmit}>Search For a Drink </button> : <button className='home-signIn' onClick={handleSignIn}>Sign In</button>}
-                {signedIn ? <button className='home-mix-button' onClick={clickMixActivateButton}>Make a Mix</button> : null}
-            </div>
-            {showSignInModal ? <SignInModal
-                username={username}
-                setUsername={setUsername}
-                password={password}
-                setPassword={setPassword}
-                handleLogInSubmit={handleLogInSubmit}
-                showSignInModal={showSignInModal}
-                setShowSignInModal={setShowSignInModal}
-                signUpFirstName={signUpFirstName}
-                setSignUpFirstName={setSignUpFirstName}
-                signUpLastName={signUpLastName}
-                setSignUpLastName={setSignUpLastName}
-                signUpUsername={signUpUsername}
-                setSignUpUsername={setSignUpUsername}
-                signUpPassword={signUpPassword}
-                setSignUpPassword={setSignUpPassword}
-                signUpPasswordConfirmation={signUpPasswordConfirmation}
-                setSignUpPasswordConfirmation={setSignUpPasswordConfirmation}
-                signUpImage={signUpImage}
-                setSignUpImage={setSignUpImage}
-                signUpDesc={signUpDesc}
-                setSignUpDesc={setSignUpDesc}
-                handleSignUpSubmit={handleSignUpSubmit}
-                handleDemoLogIn={handleDemoLogIn}
-            /> : null}
-            {/* {matchFound?<div className='no-results-container'> 
-                        <div className='no-results-found'>No Results Found</div>
-                        <div className='no-results-query'>Please Enter a Different Search Query</div>
-                    </div>:null} */}
-            {showDrinkModal ? <div className='modal-container'>
-                <DrinkModal
-                    drink={selectedDrink}
-                    setShowDrinkModal={setShowDrinkModal}
-                    setMatchFound={setMatchFound}
-                    handleFavoriteDrink={handleFavoriteDrink}
-                    handleUnfavoriteDrink={handleUnfavoriteDrink}
-                    favorites={favorites}
-                    setFavorites={setFavorites}
-                    ingredients={ingredients}
-                />
-            </div> : null}
-            {showProfileModal ? <div className='modal-container'>
-                <ProfileModal profile={user} setUser={setUser} setShowProfileModal={setShowProfileModal} handleLogOut={handleLogOut} drinks={drinks} setSelectedDrink={setSelectedDrink} setShowDrinkModal={setShowDrinkModal} />
-            </div> : null}
-            {showMixModal ? <div className='modal-container'>
-                <MixModal user={user} setShowMixModal={setShowMixModal} drink={mixToCreate} ingredients={ingredients} handleCreateDrinkSubmit={handleCreateDrinkSubmit} />
-            </div> : null}
-            {showResultsModal ? <div className='modal-container'>
-                <ResultsModal
-                    setShowResultsModal={setShowResultsModal}
-                    mixResults={mixResults}
-                    mixes={mixes}
-                    ingredients={ingredients}
-                    setSelectedDrink={setSelectedDrink}
-                    setShowDrinkModal={setShowDrinkModal}
-                    handleFavoriteDrink={handleFavoriteDrink}
-                    handleUnfavoriteDrink={handleUnfavoriteDrink}
-                    favorites={favorites}
-                    setFavorites={setFavorites}
-                />
-            </div> : null}
-            {showInformationModal ? <div className='modal-container'>
-                <InformationModal showInformationModal={showInformationModal} setShowInformationModal={setShowInformationModal} />
-            </div> : null}
-
-            {modalActive ? <div onClick={clickOffModal} className="modal-curtain"></div> : null}
-            {searchActive ? <SearchPage
-                user={user}
-                searchActive={searchActive}
-                searchResults={searchResults}
-                setSearchResults={setSearchResults}
-                searchParams={searchParams}
-                setSearchParams={setSearchParams}
-                handleSearchChange={handleSearchChange}
-                handleSearchSubmit={handleSearchSubmit}
-                scrollTop={scrollTop}
-                showDrinkModal={showDrinkModal}
-                setShowDrinkModal={setShowDrinkModal}
-                selectedDrink={selectedDrink}
-                setSelectedDrink={setSelectedDrink}
-                showProfileModal={showProfileModal}
-                favorites={favorites}
-                setFavorites={setFavorites}
-                modalActive={modalActive}
-            /> : null}
-            {mixActive ? <MixPage
-                mixes={mixes}
-                setMixes={setMixes}
-                user={user}
-                matchFound={matchFound}
-                setMatchFound={setMatchFound}
-                ingredients={ingredients}
-                setIngredients={setIngredients}
-                showDrinkModal={showDrinkModal}
-                setShowDrinkModal={setShowDrinkModal}
-                selectedDrink={selectedDrink}
-                setSelectedDrink={setSelectedDrink}
-                scrollTopMix={scrollTopMix}
-                showProfileModal={showProfileModal}
-                mixToCreate={mixToCreate}
-                setMixToCreate={setMixToCreate}
-                showMixModal={showMixModal}
-                setShowMixModal={setShowMixModal}
-                showResultsModal={showResultsModal}
-                setShowResultsModal={setShowResultsModal}
-                mixResults={mixResults}
-                setMixResults={setMixResults}
-                modalActive={modalActive}
-                showInformationModal={showInformationModal}
-                setShowInformationModal={setShowInformationModal}
-            /> : null}
-        </React.Fragment>
-    </div>
+      {displayHeader}
+      <h3 className={subtitleClass}>Welcome to</h3>
+      <div className={titleClass}><b>G<span>ut</span>H<span>u</span>b</b></div>
+      {displayHomeButtons }
+      {displaySignInModal}
+      {displayDrinkModal}
+      {displayProfileModal}
+      {displayMixModal}
+      {displayResultsModal}
+      {displayInformationModal}
+      {modalCurtain}
+      {displaySearchPage}
+      {displayMixPage}
+    </div >
   );
 }
 
